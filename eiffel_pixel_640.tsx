6bdf4739cc3c77f9c6a6a09d2af45031617d0a1e
RomaneGitHub <79529607+RomaneGitHub@users.noreply.github.com>
2021-03-09 12:27:51 +0100

    Add files via upload

--- eiffel_pixel_640.tsx
+++ eiffel_pixel_640.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="eiffel_pixel_640.png" tilewidth="32" tileheight="32" tilecount="400" columns="20">
- <image source="eiffel_pixel_640.png.png" trans="ff00ff" width="640" height="640"/>
+<tileset version="1.4" tiledversion="1.4.3" name="eiffel_pixel_640" tilewidth="32" tileheight="32" tilecount="400" columns="20">
+ <image source="../eiffel_pixel_640.png" trans="ff00ff" width="640" height="640"/>
 </tileset>
